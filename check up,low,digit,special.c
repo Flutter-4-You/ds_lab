4a1b7348ef1be8b1fc62d0d148204cc525d1cd21
Yimkumer <92160127+dukeyim@users.noreply.github.com>
2023-03-13 02:00:55 -0700

    Add files via upload

---
+++ check up,low,digit,special.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void main()
+	{
+		char a;
+		printf("Enter Any Key to Check Whether the Given Character Is an Uppercase Letter or Lowercase Letter or a Digit or a Special Character.: ");
+			scanf("%c", &a);
+
+			if (a >= 65 && a < 90)
+			{
+				printf("\nCharacter is the Uppercase\n");
+			}
+			else
+			{
+				if (a >= 97 && a <= 122)
+				{
+					printf("\nCharacter is the Lowercase\n");
+				}
+				else
+				{
+					if (a >= 48 && a <= 57)
+					{
+						printf("\nCharacter is the  Digit\n");
+					}
+					else
+					{
+						printf("\nCharacter is the  Special Symbol\n\n");
+					}
+				}
+			}
+		}
